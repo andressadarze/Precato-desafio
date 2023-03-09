3e957a9468e944293db042c08fc25e32d59d4b11
Andressa Urbano de Sousa Darze <andressa.darze@gmail.com>
2023-03-09 11:36:16 -0300

    Endpoint Signup: UserDatabase

---
+++ src/database/UserDatabase.ts
@@ -0,0 +1,30 @@
+import { IUserDB, User } from "../entities/User"
+import { BaseDatabase } from "./BaseDatabase"
+
+class UserDatabase extends BaseDatabase {
+    public static TABLE_USERS = "Precato_Forms_Answer"
+
+    public createRegister = async(user: User) => {
+        const userDB : IUserDB = {
+            id: user.getId(),
+            name: user.getName(),
+            email: user.getEmail(),
+            cpf: user.getCPF(),
+            phone: user.getPhone(),
+            createdAt: user.getCreatedAt()
+        }
+
+        await BaseDatabase.connection(UserDatabase.TABLE_USERS).insert(userDB)
+    }
+
+    public findByEmail = async(email: string) => {
+        const userDB: IUserDB[] = await BaseDatabase
+        .connection(UserDatabase.TABLE_USERS)
+        .select()
+        .where({ email })
+
+        return userDB[0]
+    }
+}
+
+export default UserDatabase
