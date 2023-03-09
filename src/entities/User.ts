e87e7a4ae4b3aa9663b99f2795ea7929299a93ac
Andressa Urbano de Sousa Darze <andressa.darze@gmail.com>
2023-03-09 11:19:27 -0300

    Entity User

---
+++ src/entities/User.ts
@@ -0,0 +1,43 @@
+export class User {
+    constructor(
+        private id: string,
+        private name: string,
+        private email: string,
+        private cpf: string,
+        private phone: string,
+        private createdAt: Date
+    ) {}
+
+    public getId = () => {
+        return this.id
+    }
+
+    public getName = () => {
+        return this.name
+    }
+
+    public getEmail = () => {
+        return this.email
+    }
+
+    public getCPF = () => {
+        return this.cpf
+    }
+
+    public getPhone = () => {
+        return this.phone
+    }
+
+    public getCreatedAt = () => {
+        return this.createdAt
+    }
+
+    public setEmail = (newEmail: string) => {
+        this.email = newEmail
+    }
+
+    public setPhone = (newPhone: string) => {
+        this.phone = newPhone
+    }
+
+}
